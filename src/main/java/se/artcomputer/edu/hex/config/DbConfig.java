9fb410a053b2a5a3101c4d40d0bb3d4f9dc396b1
Per Lundholm <per@artcomputer.se>
2023-01-22 15:32:57 +0100

    Removed Account domain as it was uninteresting.

--- src/main/java/se/artcomputer/edu/hex/config/DbConfig.java
+++ src/main/java/se/artcomputer/edu/hex/config/DbConfig.java
@@ -4,30 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import se.artcomputer.edu.hex.out.port.AccountRepository;
 import se.artcomputer.edu.hex.out.port.BookingRepository;
 
 @Service
 public class DbConfig {
     @Value("${se.artcomputer.edu.db}")
     private String dbConfig;
-
-    @Autowired
-    @Qualifier("db")
-    private AccountRepository accountRepositoryDb;
-
-    @Autowired
-    @Qualifier("stub")
-    private AccountRepository accountRepositoryStub;
-
-    public AccountRepository accountRepository() {
-        return switch (dbConfig) {
-            case "db" -> accountRepositoryDb;
-            case "stub" -> accountRepositoryStub;
-            default -> throw new RuntimeException("Bad config. Set se.artcomputer.edu.db in properties.");
-        };
-    }
-
     @Autowired
     @Qualifier("db")
     private BookingRepository bookingRepositoryDb;
--- src/main/java/se/artcomputer/edu/hex/out/adapter/db/AccountJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package se.artcomputer.edu.hex.out.adapter.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountJpaRepository extends JpaRepository<AccountEntity, Long> {
-}
